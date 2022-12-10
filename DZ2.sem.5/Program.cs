3d481e38762921a144ca1debf5e201b65888a576
yuliya-gri <yuliya.grigoreva.1986@list.ru>
2022-12-10 19:44:47 +0400

    commit DZ2

---
+++ DZ2.sem.5/Program.cs
@@ -0,0 +1,28 @@
+﻿int[] MyArray(int size)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < size; i++)
+        array[i] = new Random().Next(-99, 100);
+    return array;
+}
+void PrintArray(int[] array)
+{
+    foreach (int el in array)
+        Console.Write($"{el} ");
+    Console.WriteLine();
+}
+int ElSumm(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    {
+        sum = sum + array[i];         
+    }
+    return sum;
+}
+Console.WriteLine("Введите размерность массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array = MyArray(size);
+PrintArray(array);
+Console.WriteLine("");
+Console.WriteLine($"Сумма чисел, стоящих на нечетных позициях {ElSumm(array)}");
